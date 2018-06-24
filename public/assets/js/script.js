6ae68fc39a59a25337e58629fc97c25b9017d02a
scapps00 <hello@samanthacapps.com>
2018-06-24 16:26:16 -0400

    responsiveness added

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -17,19 +17,23 @@ $("#whiteout").click(function() {
 $(".photo").click(function() {
     $("#whiteout").css("display", "inline");
     $("#modal").css("display", "inline");
+    window.scrollTo(0, 0);
 });
 
 $(".photo2").click(function() {
     $("#whiteout").css("display", "inline");
     $("#modal2").css("display", "inline");
+    window.scrollTo(0, 0);
 });
 
 $(".photo3").click(function() {
     $("#whiteout").css("display", "inline");
     $("#modal3").css("display", "inline");
+    window.scrollTo(0, 0);
 });
 
 $(".photo4").click(function() {
     $("#whiteout").css("display", "inline");
     $("#modal4").css("display", "inline");
+    window.scrollTo(0, 0);
 });
